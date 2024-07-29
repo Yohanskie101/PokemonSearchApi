d3e5ebaeba12a23884699f035063d6b610dc5008
Yohanskie101 <125958519+Yohanskie101@users.noreply.github.com>
2024-07-30 02:51:58 +0800

    Added comment

--- main.js
+++ main.js
@@ -13,6 +13,7 @@
 // async/await
 async function Pokemon() {
   try {
+    //LAMAN NG INPUT BOX
     const pokemoninputvalue = document
       .getElementById("pokedex")
       .value.toLowerCase();
@@ -23,7 +24,10 @@ async function Pokemon() {
     if (!results.ok) {
       throw new Error("No Pokemon you search was available!");
     }
+    //COCONVERT NETONG VARIABLE GAWING JSON TYPE YUNG DATA NA MAKUKUHA SA API
     const pokedata = await results.json();
+
+    //OBJECT TO GET LAHAT NG DATA NEEDED
     const pokemongetdata = {
       name: pokedata.height,
       sprites: pokedata.sprites.front_default,
@@ -31,7 +35,7 @@ async function Pokemon() {
     };
 
     const outputDiv = document.getElementById("output");
-
+    //OUTPUT PARA MAPALABAS LAHAT NG NEEDED DATA
     outputDiv.innerHTML = `
       <h1>${pokemongetdata.name}</h1>
       <img src="${pokemongetdata.sprites}" alt="${pokemongetdata.name}" />
